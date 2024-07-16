c234c62dbb22bdef490e3f3195c9d972fbda7eb9
tech1024 <160414940+tech1024@users.noreply.github.com>
2024-07-16 02:41:32 +0000

    init

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export const PandaConfig = {
-    title: 'Panda Blog',
-    description: 'Panda Blog, a blog powered by Astro',
-    start: '2016',
-    site: 'https://astro-theme-panda.vercel.app/',
+    title: 'Initial 3000',
+    description: '一个以 3000 RMB 为初始资金的投资记录。',
+    start: '2024',
+    site: 'https://init-3k.vercel.app/',
     defaultLocale: 'en',
     navbar: [
         // {title: 'Posts', url: '/'}, // auto generated
@@ -10,7 +10,7 @@ export const PandaConfig = {
     ],
     footer: [
         { title: 'rss', url: '/rss.xml/' },
-        { title: 'contact', url: 'https://github.com/yuhangch/astro-theme-panda/issues/new' },
-        { title: 'github', url: 'https://github.com/yuhangch/astro-theme-panda' }
+        { title: 'contact', url: 'https://t.me/tech1024' },
+        { title: 'github', url: 'https://github.com/tech1024' }
     ]
 }
